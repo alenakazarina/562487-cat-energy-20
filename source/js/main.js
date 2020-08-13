fe1198e70e4b3478b736a5f78efee4d16e7044bc
Alena Kazarina <alenakazarina1@gmail.com>
2020-08-13 15:55:55 +0300

    updated slider

--- source/js/main.js
+++ source/js/main.js
@@ -37,169 +37,318 @@
   }
 
   const [sliderImageBefore, sliderImageAfter] = sliderEl.querySelectorAll(`.slider__image`);
-  const sliderControlEls = sliderEl.querySelectorAll(`.slider__control`);
+  const sliderControls = sliderEl.querySelector(`.slider__controls`);
+  const sliderControlEls = sliderControls.querySelectorAll(`.slider__control`);
   const sliderRange = sliderEl.querySelector(`.slider__range`);
   const sliderThumbler = sliderEl.querySelector(`.slider__thumb`);
   const INITIAL_X = 197;
+  const SliderState = {
+    AFTER: `after`,
+    BEFORE: `before`,
+    MIDDLE: `middle`
+  };
 
   class Slider {
     constructor() {
       this.rootEl = sliderEl;
-      this.controlEls = sliderControlEls;
+      this.controlsEl = sliderControls;
       this.controlBefore = sliderControlEls[0];
       this.controlAfter = sliderControlEls[1];
       this.rangeEl = sliderRange;
-      this.thumbler = sliderThumbler;
+      this.thumbEl = sliderThumbler;
       this.imageBefore = sliderImageBefore;
       this.imageAfter = sliderImageAfter;
-      this.active = ``;
-
+      this.active = SliderState.BEFORE;
+      this.imageX = 0;
+      this.thumbX = 0;
       this.startDragFlag = false;
+      this.startDragX = 0;
       this.minX = 0;
-      this.maxX = this.rangeEl.offsetWidth - this.thumbler.offsetWidth;
-      this.startX = INITIAL_X;
+      this.maxX = this.rangeEl.offsetWidth - this.thumbEl.offsetWidth;
+      this.minXMobile = 0;
+      this.maxXMobile = 38;
+      this.shiftX = this.thumbEl.offsetWidth;
 
-      this.handleControlClick = this.handleControlClick.bind(this);
+      this.handleControlsClickMobile = this.handleControlsClickMobile.bind(this);
+      this.handleControlsClickTablet = this.handleControlsClickTablet.bind(this);
+
+      this.handleTouchStart = this.handleTouchStart.bind(this);
+      this.handleTouchMove = this.handleTouchMove.bind(this);
+      this.handleTouchEnd = this.handleTouchEnd.bind(this);
+
       this.handleThumbMouseDown = this.handleThumbMouseDown.bind(this);
       this.handleMouseUp = this.handleMouseUp.bind(this);
       this.handleMouseMove = this.handleMouseMove.bind(this);
     }
 
-    init() {
-      const isTablet = window.matchMedia(`(min-width: 768px)`).matches;
-      for (let controlEl of this.controlEls) {
-        controlEl.addEventListener(`click`, this.handleControlClick);
+    setSlider() {
+      this.imageBefore.style.clipPath = `polygon(0 0, ${this.imageX}% 0, ${this.imageX}% 100%, 0 100%)`;
+      this.imageAfter.style.clipPath = `polygon(${this.imageX}% 0, 100% 0, 100% 100%, ${this.imageX}% 100%)`;
+      this.thumbEl.style.transform = `translateX(${this.thumbX}px)`;
+    }
+
+    setBefore() {
+      this.active = SliderState.BEFORE;
+      this.imageX = 100;
+    }
+
+    setAfter() {
+      this.active = SliderState.AFTER;
+      this.imageX = 0;
+    }
+
+    moveSlider(evtClientX) {
+      const shift = this.startDragX - evtClientX;
+      this.startDragX = evtClientX;
+      const endX = this.thumbX - shift;
+      const willMinRange = endX <= this.minX + this.shiftX / 2;
+      const willMaxRange = endX >= this.maxX - this.shiftX / 2;
+
+      if (willMinRange && shift >=0) {
+        this.thumbX = this.minX;
+        this.setBefore();
+        this.setSlider();
+        return;
       }
-      if (isTablet) {
+
+      if (willMaxRange && shift <=0) {
+        this.thumbX = this.maxX;
+        this.setAfter();
+        this.setSlider();
+        return;
+      }
+
+      const percentX = Math.round(endX * 100 / this.maxX);
+      this.active = SliderState.MIDDLE;
+      this.imageX = 100 - percentX;
+      this.thumbX = endX;
+      this.setSlider();
+    }
+
+    endDrag() {
+      if (this.startDragFlag) {
+        this.startDragFlag = false;
+      }
+    }
+
+    initMobile() {
+      this.active = SliderState.BEFORE;
+      this.imageX = 0;
+      this.thumbX = 0;
+      this.setMobileListeners(true);
+    }
+
+    initTablet() {
+      this.active = SliderState.MIDDLE;
+      this.imageX = 49;
+      this.thumbX = 197;
+      this.setTabletListeners(true);
+    }
+
+    changeToMobile() {
+      if (this.active === SliderState.AFTER) {
+        this.imageX = 0;
+        this.thumbX = this.maxXMobile;
+      } else if (this.active === SliderState.BEFORE) {
+        this.imageX = 100;
+        this.thumbX = this.minXMobile;
+      } else {
+        this.active = SliderState.BEFORE;
+        this.imageX = 100;
+        this.thumbX = this.minXMobile;
+      }
+      this.setSlider();
+      this.setTabletListeners(false);
+      this.setMobileListeners(true);
+    }
+
+    changeToTablet() {
+      if (this.active === SliderState.AFTER) {
+        this.imageX = 0;
+        this.thumbX = this.maxX;
+      } else {
+        this.imageX = 100;
+        this.thumbX = this.minX;
+      }
+      this.setSlider();
+      this.setMobileListeners(false);
+      this.setTabletListeners(true);
+    }
+
+    setMobileListeners(isActive) {
+      if (isActive) {
+        this.controlsEl.addEventListener(`click`, this.handleControlsClickMobile);
+      } else {
+        this.controlsEl.removeEventListener(`click`, this.handleControlsClickMobile);
+      }
+    }
+
+    setTabletListeners(isActive) {
+      if(isActive) {
+        this.controlsEl.addEventListener(`click`, this.handleControlsClickTablet);
+
+        this.thumbEl.addEventListener("touchstart", this.handleTouchStart);
+        this.thumbEl.addEventListener("touchmove", this.handleTouchMove);
+        this.thumbEl.addEventListener("touchend", this.handleTouchEnd);
+
         document.addEventListener('mousemove', this.handleMouseMove);
         document.addEventListener('mouseup', this.handleMouseUp);
-        this.thumbler.addEventListener(`mousedown`, this.handleThumbMouseDown);
+        this.thumbEl.addEventListener(`mousedown`, this.handleThumbMouseDown);
+      } else {
+        this.controlsEl.removeEventListener(`click`, this.handleControlsClickTablet);
+
+        this.thumbEl.removeEventListener("touchstart", this.handleTouchStart);
+        this.thumbEl.removeEventListener("touchmove", this.handleTouchMove);
+        this.thumbEl.removeEventListener("touchend", this.handleTouchEnd);
+
+        document.removeEventListener('mousemove', this.handleMouseMove);
+        document.removeEventListener('mouseup', this.handleMouseUp);
+        this.thumbEl.removeEventListener(`mousedown`, this.handleThumbMouseDown);
       }
     }
 
-    showBefore() {
-      const isTablet = window.matchMedia(`(min-width: 768px)`).matches;
-
-      if (isTablet) {
-        sliderImageBefore.style.clipPath = `polygon(0 0, 100% 0, 100% 100%, 0 100%)`;
-        sliderImageAfter.style.clipPath = `polygon(100% 0, 100% 0, 100% 100%, 100% 100%)`;
-        return;
-      }
-
-      this.imageBefore.classList.remove(`slider__image--hide`);
-      this.imageBefore.classList.add(`slider__image--show`);
-      this.imageAfter.classList.remove(`slider__image--show`);
-      this.imageAfter.classList.add(`slider__image--hide`);
-    }
-
-    showAfter() {
-      const isTablet = window.matchMedia(`(min-width: 768px)`).matches;
-
-      if (isTablet) {
-        sliderImageAfter.style.clipPath = `polygon(0 0, 100% 0, 100% 100%, 0 100%)`;
-        sliderImageBefore.style.clipPath = `polygon(0 0, 100% 0, 100% 0, 0 0)`;
-        return;
-      }
-
-      this.imageAfter.classList.remove(`slider__image--hide`);
-      this.imageAfter.classList.add(`slider__image--show`);
-      this.imageBefore.classList.remove(`slider__image--show`);
-      this.imageBefore.classList.add(`slider__image--hide`);
-    }
-
-    setThumbMax() {
-      sliderThumbler.classList.remove(`slider__thumb--before`);
-      sliderThumbler.classList.add(`slider__thumb--after`);
-      this.startX = this.maxX;
-      const isTablet = window.matchMedia(`(min-width: 768px)`).matches;
-      if (isTablet) {
-        sliderThumbler.style.transform = `translateX(${this.maxX}px)`;
-      }
-    }
-
-    setThumbMin() {
-      sliderThumbler.classList.remove(`slider__thumb--after`);
-      sliderThumbler.classList.add(`slider__thumb--before`);
-      this.startX = this.minX;
-      const isTablet = window.matchMedia(`(min-width: 768px)`).matches;
-      if (isTablet) {
-        sliderThumbler.style.transform = `translateX(${this.minX}px)`;
-      }
-    }
-
-    handleControlClick(evt) {
+    handleControlsClickMobile(evt) {
       const targetEl = evt.target;
-      const isBeforeActive = this.active === `before`;
-      const isAfterActive = this.active === `after`;
+      const isBeforeActive = this.active === SliderState.BEFORE;
 
       switch (targetEl) {
         case this.controlBefore:
-          if (isBeforeActive) {
-            return;
+          if (!isBeforeActive) {
+            this.thumbX = this.minXMobile;
+            this.setBefore();
+            this.setSlider();
           }
-          this.active = `before`;
-          this.showBefore();
-          this.setThumbMin();
           break;
+
         case this.controlAfter:
-          if (isAfterActive) {
-            return;
+          if (isBeforeActive) {
+            this.thumbX = this.maxXMobile;
+            this.setAfter();
+            this.setSlider();
           }
-          this.active = `after`;
-          this.showAfter();
-          this.setThumbMax();
+          break;
+
+        case this.rangeEl:
+          if (isBeforeActive) {
+            this.thumbX = this.maxXMobile;
+            this.setAfter();
+            this.setSlider();
+          } else {
+            this.thumbX = this.minXMobile;
+            this.setBefore();
+            this.setSlider();
+          }
           break;
         default:
-          throw Error(`Unknown slider control`);
+          return;
       }
+    }
+
+    handleControlsClickTablet(evt) {
+      const targetEl = evt.target;
+      switch (targetEl) {
+        case this.controlBefore:
+          if (this.active !== SliderState.BEFORE) {
+            this.thumbX = this.minX;
+            this.setBefore();
+            this.setSlider();
+          }
+          break;
+        case this.controlAfter:
+          if (this.active !== SliderState.AFTER) {
+            this.thumbX = this.maxX;
+            this.setAfter();
+            this.setSlider();
+          }
+          break;
+        case this.rangeEl:
+          const x = evt.offsetX;
+          if (x <= this.minX + this.shiftX / 2) {
+            this.thumbX = this.minX;
+            this.setBefore();
+            this.setSlider();
+            return;
+          }
+          if (x >= this.maxX - this.shiftX / 2) {
+            this.thumbX = this.maxX;
+            this.setAfter();
+            this.setSlider();
+            return;
+          }
+          const percentX = 100 - Math.round(x * 100 / this.maxX);
+          this.active = SliderState.MIDDLE;
+          this.imageX = percentX;
+          this.thumbX = x - this.shiftX / 2;
+          this.setSlider();
+          break;
+        default:
+          return;
+      }
+    }
+
+    handleTouchStart(evt) {
+      evt.preventDefault();
+      this.startDragFlag = true;
+
+      for (let touch of evt.changedTouches) {
+        if (touch.target === this.thumbEl) {
+          this.startDragX = touch.clientX;
+        }
+      }
+    }
+
+    handleTouchEnd() {
+      this.endDrag();
+    }
+
+    handleTouchMove(evt) {
+      evt.preventDefault();
+      if (!this.startDragFlag) {
+        return;
+      }
+
+      let evtClientX = 0;
+      for (let touch of evt.changedTouches) {
+        if (touch.target === this.thumbEl) {
+          evtClientX = touch.clientX;
+        }
+      }
+      this.moveSlider(evtClientX);
     }
 
     handleThumbMouseDown(evt) {
       evt.preventDefault();
       this.startDragFlag = true;
+      this.startDragX = evt.clientX;
     }
 
     handleMouseUp() {
-      this.startDragFlag = false;
+      this.endDrag();
     }
 
     handleMouseMove(evt) {
       if (!this.startDragFlag) {
         return;
       }
-      this.active = ``;
-      const parentX = sliderThumbler.offsetParent.offsetLeft;
-      const shift = parentX + this.startX - evt.clientX;
-      const endX = this.startX - shift;
-      const willMinRange = endX <= this.minX;
-      const willMaxRange = endX >= this.maxX;
-      const willInRange = !willMinRange && !willMaxRange;
-
-      if (willMinRange) {
-        this.active = `before`;
-        this.showBefore();
-        this.setThumbMin();
-        return;
-      }
-
-      if (willMaxRange) {
-        this.active = `after`;
-        this.showAfter();
-        this.setThumbMax();
-        return;
-      }
-
-      if (willInRange) {
-        const imageX = Math.round(endX * 100 / this.maxX);
-        sliderThumbler.style.transform = `translateX(${endX}px)`;
-        sliderImageBefore.style.clipPath = `polygon(0 0, ${100 - imageX}% 0, ${100 - imageX}% 100%, 0 100%)`;
-        sliderImageAfter.style.clipPath = `polygon(${100 - imageX}% 0, 100% 0, 100% 100%, ${100 - imageX}% 100%)`;
-        this.startX = endX;
-        return;
-      }
+      this.moveSlider(evt.clientX);
     }
   }
 
   const catSlider = new Slider();
-  catSlider.init();
+  const tabletMQ = window.matchMedia(`(min-width: 768px)`);
+  const isTablet = tabletMQ.matches;
+
+  if (isTablet) {
+    catSlider.initTablet();
+  } else {
+    catSlider.initMobile();
+  }
+
+  tabletMQ.addEventListener(`change`, () => {
+    if (tabletMQ.matches) {
+      catSlider.changeToTablet();
+    } else {
+      catSlider.changeToMobile();
+    }
+  });
 })();
